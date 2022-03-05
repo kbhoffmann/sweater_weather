b68d4cd6231970cd4e3e9fda34b20e53428554c6
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-05 14:36:57 -0700

    add webmock to spec helper, add mock data for weather api call to fixture file and weather_service_spec.

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe WeatherService do
   it 'gets current weather data for an area by geocodes' do
+    json_response = File.read('spec/fixtures/weather_data.json')
+    stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=b223e219a2cff0890dbe4fae9e6d5836&exclude=minutely,alerts&lat=39.742043&lon=-104.991531&units=imperial").
+    to_return(status: 200, body: json_response, headers: {})
+
     latitude = 	39.742043
     longitude = 	-104.991531
 
