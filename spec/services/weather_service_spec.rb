ed811dae46623d6bb77eacc9d2e6f8b1a8175178
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-05 14:27:56 -0700

    add hourly and daily weather to test

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WeatherService do
-  it 'gets weather data for an area by geocodes' do
+  it 'gets current weather data for an area by geocodes' do
     latitude = 	39.742043
     longitude = 	-104.991531
 
@@ -40,5 +40,44 @@ RSpec.describe WeatherService do
     expect(weather_data[:current][:weather].first[:description]).to be_a(String)
     expect(weather_data[:current][:weather].first).to have_key(:icon)
     expect(weather_data[:current][:weather].first[:icon]).to be_a(String)
+
+    expect(weather_data).to have_key(:hourly)
+    expect(weather_data[:hourly]).to be_an(Array)
+    expect(weather_data[:hourly][0]).to be_a(Hash)
+    expect(weather_data[:hourly][0]).to have_key(:dt)
+    expect(weather_data[:hourly][0][:dt]).to be_an(Integer)
+    expect(weather_data[:hourly][0]).to have_key(:temp)
+    expect(weather_data[:hourly][0][:temp]).to be_a(Float)
+    expect(weather_data[:hourly][0]).to have_key(:weather)
+    expect(weather_data[:hourly][0][:weather]).to be_an(Array)
+    expect(weather_data[:hourly][0][:weather][0]).to be_a(Hash)
+    expect(weather_data[:hourly][0][:weather][0]).to have_key(:description)
+    expect(weather_data[:hourly][0][:weather][0][:description]).to be_a(String)
+    expect(weather_data[:hourly][0][:weather][0]).to have_key(:icon)
+    expect(weather_data[:hourly][0][:weather][0][:icon]).to be_a(String)
+
+    expect(weather_data).to have_key(:daily)
+    expect(weather_data[:daily]).to be_an(Array)
+    expect(weather_data[:daily][0]).to be_a(Hash)
+    expect(weather_data[:daily][0]).to have_key(:dt)
+    expect(weather_data[:daily][0][:dt]).to be_an(Integer)
+    expect(weather_data[:daily][0]).to have_key(:sunrise)
+    expect(weather_data[:daily][0][:sunrise]).to be_an(Integer)
+    expect(weather_data[:daily][0]).to have_key(:sunset)
+    expect(weather_data[:daily][0][:sunset]).to be_an(Integer)
+    expect(weather_data[:daily][0]).to have_key(:temp)
+    expect(weather_data[:daily][0][:temp]).to be_a(Hash)
+    expect(weather_data[:daily][0][:temp]).to have_key(:min)
+    expect(weather_data[:daily][0][:temp][:min]).to be_a(Float)
+    expect(weather_data[:daily][0][:temp]).to have_key(:max)
+    expect(weather_data[:daily][0][:temp][:max]).to be_a(Float)
+
+    expect(weather_data[:daily][0]).to have_key(:weather)
+    expect(weather_data[:daily][0][:weather]).to be_an(Array)
+    expect(weather_data[:daily][0][:weather][0]).to be_a(Hash)
+    expect(weather_data[:daily][0][:weather][0]).to have_key(:description)
+    expect(weather_data[:daily][0][:weather][0][:description]).to be_a(String)
+    expect(weather_data[:daily][0][:weather][0]).to have_key(:icon)
+    expect(weather_data[:daily][0][:weather][0][:icon]).to be_a(String)
   end
 end
