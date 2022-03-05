a932d7cab0aa659ed2477d6ac1b5c7ee4d9014dc
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-05 16:39:06 -0700

    add .get_weather method and test

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,2 +1,6 @@
 class WeatherFacade
+  def self.current_weather(latitude, longitude)
+    current_weather_info = WeatherService.get_weather(latitude, longitude)[:current]
+    Forecast.new(current_weather_info)
+  end
 end
