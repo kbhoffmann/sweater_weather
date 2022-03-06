471e04714b5348a73b943c3aa52b75c471f6c64e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-05 17:19:18 -0700

    add test for weather facade, change name from CurrentForecast to CurrentWeather

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,6 +1,6 @@
 class WeatherFacade
   def self.current_weather(latitude, longitude)
-    current_weather_info = WeatherService.get_weather(latitude, longitude)[:current]
-    Forecast.new(current_weather_info)
+    current_weather_info = WeatherService.get_weather_data(latitude, longitude)[:current]
+    CurrentWeather.new(current_weather_info)
   end
 end
