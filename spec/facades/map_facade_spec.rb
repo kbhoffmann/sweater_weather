15c58a9e325ffa967820199433c045c19ed5d420
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-05 19:11:02 -0700

    Add map facade spec file

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MapFacade do
+  xit 'can send data to the map poro' do
+    json_response = File.read('spec/fixtures/map_geocoding_data.json')
+    stub_request(:get, "http://www.mapquestapi.com/geocoding/v1/address?key=UHerve0fkvZVNWgBwQzNhk9nhiz3gtWX&location=Denver,CO").
+    to_return(status: 200, body: json_response, headers: {})
 
-end 
+    location = "Denver,CO"
+
+    expect(MapFacade.get_lat_long(location).first).to be_a(Location)
+  end
+end
