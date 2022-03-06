65ad1ca574ccaf294013c3fe0920df45a4f3d0cc
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-06 13:56:44 -0700

    Add user model validations with spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
-class User
-end 
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+
+  has_secure_password
+end
