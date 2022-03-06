9bb1ec9add322db236a2b2dae13a5203f88a85dc
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-06 09:42:01 -0700

    Add framework of forecast serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,2 +1,47 @@
 class ForecastSerializer
-end 
+
+  def self.weather_json(current, five_day, eight_hour)
+      {
+      "data": {
+        "id": null,
+        "type": "forecast",
+        "attributes": {
+          "current_weather": {
+            "datetime": current.datetime,
+            "sunrise": current.sunrise,
+            "sunset": current.sunset,
+            "temperature": current.temperature,
+            "feels_like": current.feels_like,
+            "humidity": current.humidity,
+            "uvi": current.uvi,
+            "visibility": current.visitibility,
+            "conditions": current.conditions,
+            "icon": current.icon
+          },
+
+          "daily_weather": [
+            {
+              "date": five_day.date,
+              "sunrise": five_day.sunrise,
+              "sunset": five_day.sunset,
+              "max_temp": five_day.max_temp,
+              "min_temp": five_day.min_temp,
+              "conditions": five_day.conditions,
+              "icon": five_day.icon
+            }
+          ],
+
+          "hourly_weather": [
+            {
+              "time": eight_hour.time,
+              "temperature": eight_hour.temperature,
+              "conditions": eight_hour.conditions,
+              "icon": eight.icon
+            }
+          ]
+        }
+      }
+    }
+  end
+
+end
