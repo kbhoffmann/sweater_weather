241a05db381ef27568538880392ab52a69618e34
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-06 15:00:49 -0700

    Migrations to add api key to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_06_203602) do
+ActiveRecord::Schema.define(version: 2022_03_06_215629) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
   end
 
 end
