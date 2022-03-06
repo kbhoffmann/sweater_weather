2d9767e9a8470b88e7c018c6f4429d3ea1b50db2
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-06 16:23:53 -0700

    add session request spec file

---
+++ spec/requests/api/v1/session_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "User Session/Login Request" do
+  it 'can create a session for a user at login' do
+    login_params =
+        {
+          "email": "whatever@example.com",
+          "password": "password123"
+        }
+
+    post "/api/v1/sessions", params: login_params, as: :json
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    parsed_session_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_session_response).to be_a(Hash)
+    expect(parsed_session_response.length).to eq(1)
+    expect(parsed_session_response).to have_key(:data)
+    expect(parsed_session_response[:data]).to be_a(Hash)
+    expect(parsed_session_response[:data]).to have_key(:type)
+    expect(parsed_session_response[:data]).to have_key(:id)
+    expect(parsed_session_response[:data]).to have_key(:attributes)
+    expect(parsed_session_response[:data][:attributes]).to have_key(:email)
+    expect(parsed_session_response[:data][:attributes]).to have_key(:api_key)
+  end
+
+  xit 'returns and error and a 400 status code if unsuccessful' do
+  end 
+end
