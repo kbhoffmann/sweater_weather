8e5ef2ae47bcf2661b881221fb93b6d942064c11
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 16:21:17 -0700

    add notes in controller for next steps

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::TripsController < ApplicationController
   def show
-    # end_city_weather_at_eta = WeatherFacade.weather_on_day
     trip_info = TripFacade.get_trip_route(params[:origin], params[:destination])
+    #eta = Time.now plus trip.travel_time
+    #coords = MapFacade.get_lat_long(params[:destination])
+    #end_city_weather_at_eta = WeatherFacade.weather_at_eta(eta, coords)
   end
 end
