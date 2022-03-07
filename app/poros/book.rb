b92485908e980f289f89346c030cf1cc43449249
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 10:23:35 -0700

    Add completed book poro and test

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,5 +1,8 @@
 class Book
+  attr_reader :isbn, :title, :publisher
   def initialize(book_data)
-
+    @isbn = book_data[:isbn]
+    @title = book_data[:title]
+    @publisher = book_data[:publisher]
   end
 end
