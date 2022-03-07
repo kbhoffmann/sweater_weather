646d354d7f1ce0d23ac79099d074a106091dccf3
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 10:31:05 -0700

    edit variable nameing and call to BookSerializer

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -12,16 +12,17 @@ class Api::V1::BooksController < ApplicationController
     book_data = JSON.parse(book_api_response.body, symbolize_names: true)
 
     coords = MapFacade.get_lat_long(params[:location])
-    current_weather = WeatherFacade.current_weather(coords.latitude, coords.longitude)
+
+    forecast = WeatherFacade.current_weather(coords.latitude, coords.longitude)
     destination = params[:location]
-    summary = current_weather.conditions
-    temperature = current_weather.temperature
+    # summary = current_weather.conditions
+    # temperature = current_weather.temperature
     total_books_found = book_data[:numFound]
-    five_books = book_data[:docs].first(quantity.to_i)
-    five_books.map do |book|
-      binding.pry
-      Book.new(book)
+    five_books_data = book_data[:docs].first(quantity.to_i)
+    five_book_objects = five_books_data.map do |book|
+                  Book.new(book)
+                end
     end
-    # Book.new(book_data)
-  end
+
+    render json: BookSerializer.book_json(destination, forecast, total_books_found, five_book_objects)
 end
