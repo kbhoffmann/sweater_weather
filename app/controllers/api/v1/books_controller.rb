fc336286e3d8bf646d30ec0c6683de74c8f3b604
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 09:47:00 -0700

    comment out Webmock for now; add to books_controller to call service

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::BooksController < ApplicationController
   def index
+    book_query = params[:location]
+    quantity = params[:quantity]
+
+    book_conn = Faraday.new(url: 'http://openlibrary.org')
+
+    book_api_response = book_conn.get("/search.json?") do |request|
+      request.params['q'] = book_query
+    end
+
+    book_data = JSON.parse(book_api_response.body, symbolize_names: true)
+    binding.pry
+    #need to limit
+    Book.new(book_data)
 
   end
 end
