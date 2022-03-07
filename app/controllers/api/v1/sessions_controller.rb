e59d8300fb434a9b0a5976b12f7028ab0eb10185
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 14:09:55 -0700

    Add test and logic for error message response if incorrect password is entered

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    user.authenticate(params[:password])
-    session[:user_id] = user.id
-    render json: UserSerializer.user_json(user)
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.user_json(user)
+    else
+      render :json => { errors: "Bad Credentials" }, status: 401
+    end
   end
 end
