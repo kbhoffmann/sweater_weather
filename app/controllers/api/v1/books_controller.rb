38f2104dc932674e660c62598a60e7f02b917cd1
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 11:20:59 -0700

    clean up Books Controller before refactor

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -6,22 +6,21 @@ class Api::V1::BooksController < ApplicationController
     book_conn = Faraday.new(url: 'http://openlibrary.org')
 
     book_api_response = book_conn.get("/search.json?") do |request|
-      request.params['q'] = book_query
-    end
+                          request.params['q'] = book_query
+                        end
 
     book_data = JSON.parse(book_api_response.body, symbolize_names: true)
 
     coords = MapFacade.get_lat_long(params[:location])
 
     forecast = WeatherFacade.current_weather(coords.latitude, coords.longitude)
+
     destination = params[:location]
-    # summary = current_weather.conditions
-    # temperature = current_weather.temperature
     total_books_found = book_data[:numFound]
-    five_books_data = book_data[:docs].first(quantity.to_i)
-    five_book_objects = five_books_data.map do |book|
+    five_book_objects = book_data[:docs].first(quantity.to_i).map do |book|
                   Book.new(book)
                 end
+
     render json: BookSerializer.book_json(destination, forecast, total_books_found, five_book_objects)
   end
 end
