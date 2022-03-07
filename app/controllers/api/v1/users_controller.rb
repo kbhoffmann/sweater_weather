6f2cb3687759563ded2322c52d58ecea1bb626f5
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 13:38:40 -0700

    add error message if password and pasword confirmation do not match

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    user.save
-    render json: UserSerializer.user_json(user), status: 201
 
-    #need to add 400 error codes and message in the body if email not unique
-    #or passwords don't match etc.
+    if user.save
+      render json: UserSerializer.user_json(user), status: 201
+    else
+
+      render :json => { :errors => user.errors.full_messages }, status: 400
+    end
   end
 
   private
