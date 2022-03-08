b1010132208bd8935d8234de6e98be3b1431a0fe
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-08 11:11:42 -0700

    Add test for impossible trip

--- spec/poros/trip_spec.rb
+++ spec/poros/trip_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe Trip do
       expect(trip.travel_time).to eq("15 hours, 09 minutes")
       expect(trip.travel_hours).to eq(15)
   end
+
+  it 'has different attributes for an impossible trip' do
+    origin = "Denver, CO"
+    destination = "Zurich, Switzerland"
+
+    trip_data = {:routeError=>{:errorCode=>2, :message=>""}}
+
+    trip = Trip.new(trip_data, origin, destination)
+    expect(trip).to be_an_instance_of(Trip)
+    expect(trip.start_city).to eq("Denver, CO")
+    expect(trip.end_city).to eq("Zurich, Switzerland")
+    expect(trip.travel_time).to eq("Impossible")
+    expect(trip.travel_hours).to eq(nil)
+  end
 end
