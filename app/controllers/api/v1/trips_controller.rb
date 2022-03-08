330548e527e83f5023a88bf9d0826f3cac0a4f72
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 20:01:42 -0700

    refactore travel_hours argument in eta_weather

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::TripsController < ApplicationController
   def show
     trip_data = TripFacade.get_trip_route(params[:origin], params[:destination])
     coords = MapFacade.get_lat_long(params[:destination])
-    travel_time = trip_data.travel_hours
-    eta_weather = WeatherFacade.eta_weather(travel_time, coords.latitude, coords.longitude)
+    eta_weather = WeatherFacade.eta_weather(trip_data.travel_hours, coords.latitude, coords.longitude)
     render json: TripSerializer.road_trip_json(trip_data, eta_weather)
   end
 end
