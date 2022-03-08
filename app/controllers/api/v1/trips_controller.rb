6094898a82e171fb11057979b13010f6776ecdc8
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 18:49:13 -0700

    edit/add notes for next steps

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::TripsController < ApplicationController
   def show
     trip_info = TripFacade.get_trip_route(params[:origin], params[:destination])
+    coords = MapFacade.get_lat_long(params[:destination])
+    # coords.latitude coords.longitude
     #eta = Time.now plus trip.travel_time
-    #coords = MapFacade.get_lat_long(params[:destination])
-    #end_city_weather_at_eta = WeatherFacade.weather_at_eta(eta, coords)
+    #end_city_weather_at_eta = WeatherFacade.eta_weather(trip_travel_time, lat, long)
+    #render json: TripSerializer.roadtrip_json()
   end
 end
