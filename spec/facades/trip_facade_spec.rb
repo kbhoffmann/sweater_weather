8d83aaebada05c8221404bd508b0f2e36c22b97e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-08 11:11:57 -0700

    Add test for impossible trip

--- spec/facades/trip_facade_spec.rb
+++ spec/facades/trip_facade_spec.rb
@@ -17,4 +17,15 @@ RSpec.describe TripFacade do
     expect(trip.end_city).to eq("Milwaukee, WI")
     expect(trip.travel_time).to be_a(String)
   end
+
+  it 'sends trip data for an impossible trip to the Trip poro' do
+    swiss_trip_json_response = File.read('spec/fixtures/swiss_trip_data.json')
+    stub_request(:get, "http://www.mapquestapi.com/directions/v2/route?from=Denver,%20CO&key=UHerve0fkvZVNWgBwQzNhk9nhiz3gtWX&to=Zurich,%20Switzerland").
+    to_return(status: 200, body: swiss_trip_json_response, headers: {})
+
+    origin = "Denver, CO"
+    destination = "Zurich, Switzerland"
+
+    expect(TripFacade.get_trip_route(origin, destination)).to be_a(Trip)
+  end
 end
