c385da1e92a4c42286b373649b61fbb39f2a45b8
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-08 09:41:37 -0700

    Add error if API key not provided

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::TripsController < ApplicationController
   def show
-    trip_data = TripFacade.get_trip_route(params[:origin], params[:destination])
-    coords = MapFacade.get_lat_long(params[:destination])
-    eta_weather = WeatherFacade.eta_weather(trip_data.travel_hours, coords.latitude, coords.longitude)
-    render json: TripSerializer.road_trip_json(trip_data, eta_weather)
+    if params[:api_key].length == 32
+      trip_data = TripFacade.get_trip_route(params[:origin], params[:destination])
+      coords = MapFacade.get_lat_long(params[:destination])
+      eta_weather = WeatherFacade.eta_weather(trip_data.travel_hours, coords.latitude, coords.longitude)
+      render json: TripSerializer.road_trip_json(trip_data, eta_weather)
+    else
+      render :json => { errors: "You Must Provide an API key" }, status: 401
+    end
   end
 end
--- spec/requests/api/v1/trip_request_spec.rb
+++ spec/requests/api/v1/trip_request_spec.rb
@@ -14,8 +14,6 @@ RSpec.describe 'User Roadtrip' do
     stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=b223e219a2cff0890dbe4fae9e6d5836&exclude=minutely,alerts&lat=43.041072&lon=-87.909421&units=imperial").
     to_return(status: 200, body: eta_weather_json_response, headers: {})
 
-
-
     user =
     {
       "email": "kerri@example.com",
@@ -73,5 +71,86 @@ RSpec.describe 'User Roadtrip' do
     expect(parsed_trip_data[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
     expect(parsed_trip_data[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
     expect(parsed_trip_data[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+  end
+
+  it 'requires an API key is provided in the request, otherwise returns a 401 error code' do
+    trip_json_response = File.read('spec/fixtures/trip_route_data.json')
+    stub_request(:get, "http://www.mapquestapi.com/directions/v2/route?from=Denver,%20CO&key=UHerve0fkvZVNWgBwQzNhk9nhiz3gtWX&to=Milwaukee,%20WI").
+    to_return(status: 200, body: trip_json_response, headers: {})
+
+    coords_json_response = File.read('spec/fixtures/coords_destination_data.json')
+    stub_request(:get, "http://www.mapquestapi.com/geocoding/v1/address?key=UHerve0fkvZVNWgBwQzNhk9nhiz3gtWX&location=Milwaukee,%20WI").
+    to_return(status: 200, body: coords_json_response, headers: {})
+
+    eta_weather_json_response = File.read('spec/fixtures/eta_weather_data.json')
+    stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=b223e219a2cff0890dbe4fae9e6d5836&exclude=minutely,alerts&lat=43.041072&lon=-87.909421&units=imperial").
+    to_return(status: 200, body: eta_weather_json_response, headers: {})
+
+    user =
+    {
+      "email": "kerri@example.com",
+      "password": "password123",
+      "password_confirmation": "password123"
+    }
+    post "/api/v1/users", params: user, as: :json
+
+    login_params =
+        {
+          "email": "kerri@example.com",
+          "password": "password123"
+        }
+
+    post "/api/v1/sessions", params: login_params, as: :json
+
+    user = User.find_by(email: login_params[:email])
+
+    trip_params =
+        {
+          "origin": "Denver, CO",
+          "destination": "Milwaukee, WI",
+          "api_key": ""
+        }
+
+    post "/api/v1/road_trip", params: trip_params, as: :json
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+
+    expect(response.status).to eq(401)
+    expect(parsed_response[:errors]).to eq("You Must Provide an API key")
+  end
+
+  xit 'does not return a weather block for impossible route and returns impossible for travel time' do
+    user =
+    {
+      "email": "kerri@example.com",
+      "password": "password123",
+      "password_confirmation": "password123"
+    }
+    post "/api/v1/users", params: user, as: :json
+
+    login_params =
+        {
+          "email": "kerri@example.com",
+          "password": "password123"
+        }
+
+    post "/api/v1/sessions", params: login_params, as: :json
+
+    user = User.find_by(email: login_params[:email])
+
+    trip_params =
+        {
+          "origin": "Denver, CO",
+          "destination": "Zurich, Switzerland",
+          "api_key": "#{user.api_key}"
+        }
+
+    post "/api/v1/road_trip", params: trip_params, as: :json
+
+    parsed_trip_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
   end
 end
