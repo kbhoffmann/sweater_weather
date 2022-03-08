125d00cb2a8b27ecf4660173fdb4f8ea43aa430f
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-07 18:47:29 -0700

    remove pry and fix spacing

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -1,8 +1,6 @@
 class TripFacade
   def self.get_trip_route(origin, destination)
     trip_data = MapService.get_trip_route(origin, destination)[:route]
-    binding.pry
-
     Trip.new(trip_data, origin, destination)
   end
 end
