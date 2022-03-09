43f3994d204c15209339e31100434b5e6bcb7852
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-09 07:25:36 -0700

    add .format_time and test to format the time to string with hours and minutes spelled out

--- app/poros/trip.rb
+++ app/poros/trip.rb
@@ -6,6 +6,7 @@ class Trip
     @end_city = destination
     @travel_time = route_available(trip_data)
     @travel_hours = route_available(trip_data)
+
   end
 
   def route_available(trip_data)
@@ -13,8 +14,14 @@ class Trip
       @travel_time = "Impossible"
       @travel_hours = "Impossible"
     else
-      @travel_time = trip_data[:formattedTime]
+      @travel_time = format_time(trip_data)
       @travel_hours = trip_data[:realTime] / 3600
     end
+  end
+
+  def format_time(trip_data)
+    hours = trip_data[:formattedTime][0] + trip_data[:formattedTime][1]
+    minutes = trip_data[:formattedTime][3] + trip_data[:formattedTime][4]
+    "#{hours} hours, #{minutes} minutes"
   end
 end
--- spec/poros/trip_spec.rb
+++ spec/poros/trip_spec.rb
@@ -11,12 +11,13 @@ RSpec.describe Trip do
          :formattedTime=>"14:59:49",
          :routeError=>{:errorCode=> -400, :message=>""}
                }
+
       trip = Trip.new(trip_data, origin, destination)
 
       expect(trip).to be_an_instance_of(Trip)
       expect(trip.start_city).to eq("Denver, CO")
       expect(trip.end_city).to eq("Milwaukee, WI")
-      expect(trip.travel_time).to eq("14:59:49")
+      expect(trip.travel_time).to eq("14 hours, 59 minutes")
       expect(trip.travel_hours).to eq(15)
   end
 
@@ -27,10 +28,27 @@ RSpec.describe Trip do
     trip_data = {:routeError=>{:errorCode=>2, :message=>""}}
 
     trip = Trip.new(trip_data, origin, destination)
+
     expect(trip).to be_an_instance_of(Trip)
     expect(trip.start_city).to eq("Denver, CO")
     expect(trip.end_city).to eq("Zurich, Switzerland")
     expect(trip.travel_time).to eq("Impossible")
     expect(trip.travel_hours).to eq("Impossible")
+  end
+
+  it 'can format time to human readable string' do
+    origin = "Denver, CO"
+    destination = "Milwaukee, WI"
+
+    trip_data = {
+       :distance=>1047.017,
+       :realTime=>54577,
+       :formattedTime=>"14:59:49",
+       :routeError=>{:errorCode=> -400, :message=>""}
+             }
+
+    trip = Trip.new(trip_data, origin, destination)
+
+    expect(trip.format_time(trip_data)).to eq("14 hours, 59 minutes")
   end
 end
