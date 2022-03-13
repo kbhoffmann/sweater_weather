45d98909954874b3a440852159b907043a81038f
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-13 13:38:27 -0600

    refactor so only one api call is made to find weather for a destination in the weather controller

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::WeatherController < ApplicationController
   def show
     coords = MapFacade.get_lat_long(params[:location])
-    current = WeatherFacade.current_weather(coords.latitude, coords.longitude)
-    five_day = WeatherFacade.five_day_weather(coords.latitude, coords.longitude)
-    eight_hour = WeatherFacade.next_eight_hours(coords.latitude, coords.longitude)
-    render json: ForecastSerializer.weather_json(current, five_day, eight_hour)
+    weather_data = WeatherFacade.get_weather(coords.latitude, coords.longitude)
+    render json: ForecastSerializer.weather_json(weather_data)
   end
 end
--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,29 +1,35 @@
 class WeatherFacade
   def self.get_weather(latitude, longitude)
-    WeatherService.get_weather_data(latitude, longitude)
+   weather_data = WeatherService.get_weather_data(latitude, longitude)
+
+   weather_hash = Hash.new
+   weather_hash[:current_weather] = current_weather(weather_data)
+   weather_hash[:daily_weather] = daily_weather(weather_data)
+   weather_hash[:hourly_weather] = hourly_weather(weather_data)
+   weather_hash
   end
 
-  def self.current_weather(latitude, longitude)
-    current_weather_data = get_weather(latitude, longitude)[:current]
-    CurrentWeather.new(current_weather_data)
+  def self.current_weather(weather_data)
+    current_weather = (weather_data)[:current]
+    CurrentWeather.new(current_weather)
   end
 
-  def self.daily_weather(latitude, longitude)
-    days_weather_data = get_weather(latitude, longitude)[:daily].first(5)
-    days_weather_data.map do |day_data|
+  def self.daily_weather(weather_data)
+    days_data = (weather_data)[:daily].first(5)
+    days_data.map do |day_data|
       DailyWeather.new(day_data)
     end
   end
 
-  def self.hourly_weather(latitude, longitude)
-      hours_weather_data = get_weather(latitude, longitude)[:hourly].first(8)
-      hours_weather_data.map do |hour_data|
+  def self.hourly_weather(weather_data)
+      hours_data = (weather_data)[:hourly].first(8)
+      hours_data.map do |hour_data|
       HourlyWeather.new(hour_data)
     end
   end
 
   def self.eta_weather(travel_time, latitude, longitude)
-    eta_hour_data = get_weather(latitude, longitude)[:hourly].first(travel_time).last
+    eta_hour_data = WeatherService.get_weather_data(latitude, longitude)[:hourly].first(travel_time).last
     DestinationWeather.new(eta_hour_data)
   end
 end
