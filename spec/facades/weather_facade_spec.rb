bc67221ff4d96b610f02c9ba68f85b94122a9244
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-14 09:15:11 -0600

    edit tests to reflect change in arguments being used

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -17,10 +17,9 @@ RSpec.describe WeatherFacade do
     stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=b223e219a2cff0890dbe4fae9e6d5836&exclude=minutely,alerts&lat=39.738453&lon=-104.984853&units=imperial").
     to_return(status: 200, body: json_response, headers: {})
 
-    latitude = 39.738453
-    longitude = -104.984853
+    weather_data = JSON.parse(json_response, symbolize_names: true)
 
-    expect(WeatherFacade.current_weather(latitude, longitude)).to be_a(CurrentWeather)
+    expect(WeatherFacade.current_weather(weather_data)).to be_a(CurrentWeather)
   end
 
   it 'sends information to the Daily Weather poro' do
@@ -28,11 +27,10 @@ RSpec.describe WeatherFacade do
     stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=b223e219a2cff0890dbe4fae9e6d5836&exclude=minutely,alerts&lat=39.742043&lon=-104.991531&units=imperial").
     to_return(status: 200, body: json_response, headers: {})
 
-    latitude = 39.742043
-    longitude = -104.991531
+    weather_data = JSON.parse(json_response, symbolize_names: true)
 
-    expect(WeatherFacade.daily_weather(latitude, longitude).first).to be_a(DailyWeather)
-    expect(WeatherFacade.daily_weather(latitude, longitude).length).to eq(5)
+    expect(WeatherFacade.daily_weather(weather_data).first).to be_a(DailyWeather)
+    expect(WeatherFacade.daily_weather(weather_data).length).to eq(5)
   end
 
   it 'sends information to the Hourly Weather poro' do
@@ -40,11 +38,10 @@ RSpec.describe WeatherFacade do
     stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=b223e219a2cff0890dbe4fae9e6d5836&exclude=minutely,alerts&lat=39.742043&lon=-104.991531&units=imperial").
     to_return(status: 200, body: json_response, headers: {})
 
-    latitude = 39.742043
-    longitude = -104.991531
+    weather_data = JSON.parse(json_response, symbolize_names: true)
 
-    expect(WeatherFacade.hourly_weather(latitude, longitude).first).to be_an(HourlyWeather)
-    expect(WeatherFacade.hourly_weather(latitude, longitude).length).to eq(8)
+    expect(WeatherFacade.hourly_weather(weather_data).first).to be_an(HourlyWeather)
+    expect(WeatherFacade.hourly_weather(weather_data).length).to eq(8)
   end
 
   it 'sends information to the Hourly Weather poro for trip eta' do
